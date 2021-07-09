f35a536a6c8e3106c74fb6b9ea0ce3d185f88a59
Aliaksandr Martsun <aliaksandr.martsun@gmail.com>
2021-07-09 15:25:09 +0300

    Homework 16

--- Task 1.py
+++ Task 1.py
@@ -1,112 +1,75 @@
-class Time:
-    def __init__(self, hours=0, minutes=0, seconds=0, string_time='', other_class_object=None):
-        if hours != 0 and minutes != 0 and seconds != 0:
-            self.hours = hours
-            self.minutes = minutes
-            self.seconds = seconds
-        elif string_time != '':
-            list_time = string_time.split(':')
-            self.hours = list_time[0]
-            self.minutes = list_time[1]
-            self.seconds = list_time[2]
+class Tomato:
+    states = ['Pod', 'Green', 'Yellow', 'Ripe']
+
+    def __init__(self, index, state=states[0]):
+        self._index = index
+        self._state = state
+
+    def get_state(self):
+        return self._state
+
+    def grow(self):
+        index = Tomato.states.index(self._state)
+        if index < (len(Tomato.states)-1):
+            self._state = Tomato.states[index + 1]
         else:
-            self.hours = other_class_object.hours
-            self.minutes = other_class_object.minutes
-            self.seconds = other_class_object.seconds
+            return 'Томат уже созрел'
+        return self._state
 
-    def __eq__(self, other):
-        return (
-                self.hours == other.hours and
-                self.minutes == other.minutes and
-                self.seconds == other.seconds
-        )
-
-    def __ne__(self, other):
-        return (
-                self.hours != other.hours or
-                self.minutes != other.minutes or
-                self.seconds != other.seconds
-        )
-
-    def __lt__(self, other):
-        if self.hours < other.hours:
-            return True
-        elif self.hours == other.hours and self.minutes < other.minutes:
-            return True
-        elif self.hours == other.hours and self.minutes == other.minutes and self.seconds < other.seconds:
+    def is_ripe(self):
+        if self._state == 'Ripe':
             return True
         else:
             return False
-
-    def __gt__(self, other):
-        if self.hours > other.hours:
-            return True
-        elif self.hours == other.hours and self.minutes > other.minutes:
-            return True
-        elif self.hours == other.hours and self.minutes == other.minutes and self.seconds > other.seconds:
-            return True
-        else:
-            return False
-
-    def __le__(self, other):
-        if self.hours < other.hours:
-            return True
-        elif self.hours == other.hours and self.minutes < other.minutes:
-            return True
-        elif self.hours == other.hours and self.minutes == other.minutes and self.seconds <= other.seconds:
-            return True
-        else:
-            return False
-
-    def __ge__(self, other):
-        if self.hours > other.hours:
-            return True
-        elif self.hours == other.hours and self.minutes > other.minutes:
-            return True
-        elif self.hours == other.hours and self.minutes == other.minutes and self.seconds >= other.seconds:
-            return True
-        else:
-            return False
-
-    def __sub__(self, other):
-        return Time(
-            self.hours - other.hours,
-            self.minutes - other.minutes,
-            self.seconds - other.seconds,
-        )
-
-    def __add__(self, other):
-        return Time(
-            self.hours + other.hours,
-            self.minutes + other.minutes,
-            self.seconds + other.seconds
-        )
-
-    def __mul__(self, value):
-        return Time(
-            self.hours * value,
-            self.minutes * value,
-            self.seconds * value
-        )
 
     def __str__(self):
-        hours = int(self.hours)
-        minutes = int(self.minutes)
-        seconds = int(self.seconds)
-        while hours > 23 or minutes > 59 or seconds > 59:
-            while hours > 23:
-                hours -= 24
-            while minutes > 59:
-                hours += 1
-                minutes = minutes - 60
-            while seconds > 59:
-                minutes += 1
-                seconds = seconds - 60
-        if hours < 10:
-            hours = f'0{hours}'
-        if minutes < 10:
-            minutes = f'0{minutes}'
-        if seconds < 10:
-            seconds = f'0{seconds}'
+        return f'Томат на стадии: {self._state}'
 
-        return f'{hours}:{minutes}:{seconds}'
+
+class TomatoBush:
+    def __init__(self, amount):
+        self._tomatoes = []
+        for i in range(amount):
+            self._tomatoes.append(Tomato(input('Vvedite index: ')))
+
+    def all_grow(self):
+        for i in self._tomatoes:
+            i.grow()
+
+    def all_ripe(self):
+        for i in self._tomatoes:
+            if not i.is_ripe():
+                return False
+        return True
+
+    def give_away_all(self):
+        self._tomatoes = []
+
+    def __str__(self):
+        for i in self._tomatoes:
+            print(i)
+        return ''
+
+
+class Gardener:
+    def __init__(self, name, tomato):
+        self._name = name
+        self._plant = tomato
+
+    def work(self):
+        print(f'Садовник работает, томат вырастает из {self._plant.state} в {self._plant.grow()}.')
+
+    def harvest(self):
+        if self._plant.is_ripe():
+            print('Пора собирать урожай.')
+        else:
+            print('Томат еще не вырос.')
+
+
+tomato1 = Tomato('1')
+print(tomato1.state)
+gardener1 = Gardener('John', tomato1)
+gardener1.work()
+gardener1.work()
+gardener1.work()
+gardener1.work()
